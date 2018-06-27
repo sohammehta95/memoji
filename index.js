054e6f6d13340181bb4c6dba98e5c541c0587cbb
sohammehta95 <32079958+sohammehta95@users.noreply.github.com>
2018-06-26 23:52:59 -0700

    1.0.2

--- index.js
+++ index.js
@@ -3,8 +3,25 @@ exports.printMsg = function() {
 }
 
 exports.memify = function(obj) {
-	if(obj.gender=='male')
-		console.log("Male");
-	else
-		console.log("Female");
+	if(obj.gender=="male")
+		console.log("nay");       
+}
+
+module.exports = function(){
+	console.log("             .-\"\"\"-.                   ");
+    console.log("             / .===. \                   ");
+    console.log("             \/ 6 6 \/                  ");
+    console.log("             ( \___/ )                  ");
+    console.log("_________ooo__\_____/______________      ");
+    console.log("/                                   \       ");
+    console.log("|   								   |      ");
+    console.log("|     Memojis Coming Soon     		   |      ");
+    console.log("| 									   |      ");
+    console.log(" \_______________________ooo_________/       ");
+    console.log("              |  |  |");
+    console.log("              |_ | _|");
+    console.log("              |  |  |");
+	console.log("              |__|__|");
+	console.log("              /-'Y'-\ ");
+	console.log("             (__/ \__)");      
 }
