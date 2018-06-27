87b5400c9ae1ecae2784a4331831db082385e4bd
sohammehta95 <32079958+sohammehta95@users.noreply.github.com>
2018-06-27 00:28:52 -0700

    1.0.4

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 
-module.exports.memify = function(obj) {
+exports.memify = function(obj) {
 	console.log("hi");     
 }
 
-
-module.exports = function(){
-	console.log("             .-\"\"\"-.                   ");
-    console.log("             / .===. \                   ");
-    console.log("             \/ 6 6 \/                  ");
-    console.log("             ( \___/ )                  ");
-    console.log("_________ooo__\_____/_________________     ");
-    console.log("|                                     | ");
-    console.log("|                                     |");
-    console.log("|     Memojis Are Amazing!            |");
-    console.log("| 									   |      ");
-    console.log(" \_______________________ooo_________/       ");
-    console.log("              |  |  |");
-    console.log("              |_ | _|");
-    console.log("              |  |  |");
-	console.log("              |__|__|");
-	console.log("              /-'Y'-\ ");
-	console.log("             (__/ \__)");      
+var str = "hi \ yo \ lala";
+exports = function(){
+     console.log(str);
 }
+
+
+    // console.log("             .-\"\"\"-.                   ");
+    // console.log("             / .===. \                   ");
+    // console.log("             \/ 6 6 \/                  ");
+    // console.log("             ( \___/ )                  ");
+    // console.log("_________ooo__\_____/_________________     ");
+    // console.log("|                                     | ");
+    // console.log("|                                     |");
+    // console.log("|     Memojis Are Amazing!            |");
+    // console.log("|                                     |      ");
+    // console.log(" \_______________________ooo_________/       ");
+    // console.log("              |  |  |");
+    // console.log("              |_ | _|");
+    // console.log("              |  |  |");
+    // console.log("              |__|__|");
+    // console.log("              /-'Y'-\ ");
+    // console.log("             (__/ \__)"); 
